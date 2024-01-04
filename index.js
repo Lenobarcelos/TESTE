c1e225a6f28a1e3f49f267fe9ba8b787f71b65ac
LenoBarcelos <24229977-LenoBarcelos@users.noreply.replit.com>
2024-01-04 20:48:38 +0000

    Aulas Javascript

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 //PRIMEIRA AULA JAVASCRIPT//
-console.log ("Olá, mundo!!!");
+console.log("Olá, mundo!!!");
 console.log("//PRIMEIRA AULA JAVASCRIPT//");
 
 //SEGUNDA AULA JAVASCRIPT//
-console.log ("//SEGUNDA AULA JAVASCRIPT//");
-console.log("Mostrando de 1 até 10"); 
+console.log("//SEGUNDA AULA JAVASCRIPT//");
+console.log("Mostrando de 1 até 10");
 for (i = 1; i <= 10; i++) {
   console.log("i = ", i);
 }
 console.log("O programa finaliza aqui!!!");
 
 //TERCEIRA AULA JAVASCRIPT//
-console.log ("//TERCEIRA AULA JAVASCRIPT//");
+console.log("//TERCEIRA AULA JAVASCRIPT//");
 var nome = "Leno";
 var idade = 31;
 console.log(nome, "tem", idade, "anos");
-
-console.log ("  ");
