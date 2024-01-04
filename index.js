589f304e841ce7109fa183322a23d753a90a999f
LenoBarcelos <24229977-LenoBarcelos@users.noreply.replit.com>
2024-01-04 21:43:51 +0000

    Inserção de dados

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-//CALCULO SALARIO//
-var salario_bruto  = 1410;
-var inss = salario_bruto * 0.11;
-var salario_liquido = salario_bruto - inss;
-console.log("Seu salário líquido é de: R$", salario_liquido);
+// PARA INSERIR DADOS//
+const prompt = require('prompt-sync')(); 
+valor1 = prompt("Digite o primeiro valor: ");
+valor2 = prompt("Digite o segundo valor: ");
+soma = valor1 + valor2;
+console.log("\nResultado =", soma);
 
+// PARA INSERIR NÚMEROS //
+const prompt = require('prompt-sync')(); 
+numero1 = parseInt(prompt("Digite o número 1: "));
+numero2 = parseInt(prompt("Digite o número 2: "));
+soma = numero1 + numero2;
+console.log("\nResultado = ", soma);  
